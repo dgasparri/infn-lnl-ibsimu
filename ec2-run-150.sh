dc8ebbbf5d9988292c0ee7062d42ddb0daa909f1
dgasparri <553197+dgasparri@users.noreply.github.com>
2021-05-03 18:46:02 +0000

    simu 150 results 2o batch

--- ec2-run-150.sh
+++ ec2-run-150.sh
@@ -1,7 +1,7 @@
-../ibsimu_client/bin/simulation --run simu.150.1-EC2.ground0V-focus--1000V
-../ibsimu_client/bin/simulation --run simu.150.1-EC2.ground0V-focus--1500V
-../ibsimu_client/bin/simulation --run simu.150.1-EC2.ground0V-focus--2000V
-../ibsimu_client/bin/simulation --run simu.150.1-EC2.ground0V-focus--500V
+# ../ibsimu_client/bin/simulation --run simu.150.1-EC2.ground0V-focus--1000V
+# ../ibsimu_client/bin/simulation --run simu.150.1-EC2.ground0V-focus--1500V
+# ../ibsimu_client/bin/simulation --run simu.150.1-EC2.ground0V-focus--2000V
+# ../ibsimu_client/bin/simulation --run simu.150.1-EC2.ground0V-focus--500V
 ../ibsimu_client/bin/simulation --run simu.150.1-EC2.ground0V-focus-0V
 ../ibsimu_client/bin/simulation --run simu.150.1-EC2.ground0V-focus-1000V
 ../ibsimu_client/bin/simulation --run simu.150.1-EC2.ground0V-focus-1500V
